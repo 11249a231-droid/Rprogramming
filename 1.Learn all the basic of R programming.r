2a3fa2f7e2c6ca404f3166491bae8563e7602655
11249a231-droid <11249a231@kanchiuniv.ac.in>
2026-04-21 02:07:47 +0530

    Create 1.Learn all the basic of R programming.r

---
+++ 1.Learn all the basic of R programming.r
@@ -0,0 +1,95 @@
+1.1
+Student Information System
+R Program:
+roll_no <- 071 # Integer
+name <- "rebel" # Character
+cgpa <- 7.14 # Numeric
+pass <- TRUE# Logical
+roll_no
+name
+cgpa
+pass
+
+1.6
+Employee Salary Record
+R program:
+emp_id <- 1064
+salary <- 230790.24
+department <- "CSE"
+is_permanent <- TRUE
+emp_id
+salary
+department
+is_permanent
+
+2.6
+Online Shopping Cart
+R program:
+product_name <- "Moblie"
+price <- 71230
+stock_available <- 07
+discount_eligible <- FALSE
+product_name
+price
+stock_available
+discount_eligible
+
+7.9
+Weather Monitoring System
+R program:
+city <- "Hyderabad"
+temperature <- 49.7
+rain <- FALSE
+wind_speed <- 14.4
+city
+temperature
+rain
+wind_speed
+
+1.8
+Examination Result Processing
+R program:
+subject <- "Social"
+total_marks <- 98
+marks_obtained <- 74
+result <- marks_obtained >= 74
+subject
+total_marks
+marks_obtained
+result
+
+9.0
+Banking Application
+R program:
+account_no <- 42412346
+holder_name <- "Rebel"
+balance <- 32989.90
+account_active <- TRUE
+account_no
+holder_name
+balance
+account_active
+
+1.8
+Movie Rating System
+R program:
+movie_title <- "SALAAR"
+rating <- 9.9
+reviews <- 1402
+available <- TRUE
+movie_title
+rating
+reviews
+available
+
+3.5
+Traﬃc Monitoring System
+R program:
+vehicle_count <- 240
+location <- "Highway"
+avg_speed <- 30
+congested <- TRUE
+vehicle_count
+location
+avg_speed
+congested
